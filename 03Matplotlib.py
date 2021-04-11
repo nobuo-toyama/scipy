b554d01e87d061a3fc18bbb4b419de9e6d6b5ea6
nobuo-toyama <nobuo.toyama@gmail.com>
2021-04-11 14:08:47 +0900

    training

--- 03Matplotlib.py
+++ 03Matplotlib.py
@@ -319,3 +319,579 @@ ax2.set_ylabel('Undamped')
 plt.show()
 
 # ==== Images
+# The most common way to plot images in Matplotlib is with imshow.
+
+import numpy as np
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+import matplotlib.cbook as cbook
+from matplotlib.path import Path
+from matplotlib.patches import PathPatch
+
+# Fixing random state for reproducibility
+np.random.seed(19680801)
+
+# First we'll generate a simple bivariate normal distribution.
+delta = 0.025
+x = y = np.arange(-3.0, 3.0, delta)
+X, Y = np.meshgrid(x, y)
+Z1 = np.exp(-X**2 - Y**2)
+Z2 = np.exp(-(X - 1)**2 - (Y - 1)**2)
+Z = (Z1 - Z2) * 2
+
+fig, ax = plt.subplots()
+im = ax.imshow(Z, interpolation='bilinear', cmap=cm.RdYlGn,
+               origin='lower', extent=[-3, 3, -3, 3],
+               vmax=abs(Z).max(), vmin=-abs(Z).max())
+plt.show()
+
+# ---- It is also possible to show images of pictures.
+
+# A sample image
+with cbook.get_sample_data('grace_hopper.jpg') as image_file:
+    image = plt.imread(image_file)
+
+fig, ax = plt.subplots()
+ax.imshow(image)
+ax.axis('off')  # clear x-axis and y-axis
+
+# ---- And another image
+# Data are 256x256 16-bit integers.
+w, h = 256, 256
+with cbook.get_sample_data('s1045.ima.gz') as datafile:
+    s = datafile.read()
+A = np.frombuffer(s, np.uint16).astype(float).reshape((w, h))
+
+fig, ax = plt.subplots()
+extent = (0, 25, 0, 25)
+im = ax.imshow(A, cmap=plt.cm.hot, origin='upper', extent=extent)
+
+markers = [(15.9, 14.5), (16.8, 15)]
+x, y = zip(*markers)
+ax.plot(x, y, 'o')
+ax.set_title('MRI')
+plt.show()
+
+# ---- Interpolating images
+A = np.random.rand(5, 5)
+
+fig, axs = plt.subplots(1, 3, figsize=(10, 3))
+for ax, interp in zip(axs, ['nearest', 'bilinear', 'bicubic']):
+    ax.imshow(A, interpolation=interp)
+    ax.set_title(interp.capitalize())
+    ax.grid(True)
+
+plt.show()
+
+# ==== Contouring and pseudocolor
+
+# ==== Histograms
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(19680801)
+
+# example data
+mu = 100  # mean of distribution
+sigma = 15  # standard deviation of distribution
+x = mu + sigma * np.random.randn(437)
+
+num_bins = 50
+
+fig, ax = plt.subplots()
+
+# the histogram of the data
+n, bins, patches = ax.hist(x, num_bins, density=True)
+
+# add a 'best fit' line
+y = ((1 / (np.sqrt(2 * np.pi) * sigma)) *
+     np.exp(-0.5 * (1 / sigma * (bins - mu))**2))
+ax.plot(bins, y, '--')
+ax.set_xlabel('Smarts')
+ax.set_ylabel('Probability density')
+ax.set_title(r'Histogram of IQ: $\mu=100$, $\sigma=15$')
+
+# Tweak spacing to prevent clipping of ylabel
+fig.tight_layout()
+plt.show()
+
+# ==== Paths
+import matplotlib.path as mpath
+import matplotlib.patches as mpatches
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots()
+
+Path = mpath.Path
+path_data = [
+    (Path.MOVETO, (1.58, -2.57)),
+    (Path.CURVE4, (0.35, -1.1)),
+    (Path.CURVE4, (-1.75, 2.0)),
+    (Path.CURVE4, (0.375, 2.0)),
+    (Path.LINETO, (0.85, 1.15)),
+    (Path.CURVE4, (2.2, 3.2)),
+    (Path.CURVE4, (3, 0.05)),
+    (Path.CURVE4, (2.0, -0.5)),
+    (Path.CLOSEPOLY, (1.58, -2.57)),
+    ]
+codes, verts = zip(*path_data)
+path = mpath.Path(verts, codes)
+patch = mpatches.PathPatch(path, facecolor='r', alpha=0.5)
+ax.add_patch(patch)
+
+# plot control points and connecting lines
+x, y = zip(*path.vertices)
+line, = ax.plot(x, y, 'go-')
+
+ax.grid()
+ax.axis('equal')
+plt.show()
+
+# ==== Three-dimensional plotting
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator
+import numpy as np
+
+fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+
+# Make data.
+X = np.arange(-5, 5, 0.25)
+Y = np.arange(-5, 5, 0.25)
+X, Y = np.meshgrid(X, Y)
+R = np.sqrt(X**2 + Y**2)
+Z = np.sin(R)
+
+# Plot the surface.
+surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+
+# Customize the z axis.
+ax.set_zlim(-1.01, 1.01)
+ax.zaxis.set_major_locator(LinearLocator(10))
+# A StrMethodFormatter is used automatically
+ax.zaxis.set_major_formatter('{x:.02f}')
+
+# Add a color bar which maps values to colors.
+fig.colorbar(surf, shrink=0.5, aspect=5)
+
+plt.show()
+
+# ==== Streamplot
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+
+w = 3
+Y, X = np.mgrid[-w:w:100j, -w:w:100j]
+U = -1 - X**2 + Y
+V = 1 + X - Y**2
+speed = np.sqrt(U**2 + V**2)
+
+fig = plt.figure(figsize=(7, 9))
+gs = gridspec.GridSpec(nrows=3, ncols=2, height_ratios=[1, 1, 2])
+
+#  Varying density along a streamline
+ax0 = fig.add_subplot(gs[0, 0])
+ax0.streamplot(X, Y, U, V, density=[0.5, 1])
+ax0.set_title('Varying Density')
+
+# Varying color along a streamline
+ax1 = fig.add_subplot(gs[0, 1])
+strm = ax1.streamplot(X, Y, U, V, color=U, linewidth=2, cmap='autumn')
+fig.colorbar(strm.lines)
+ax1.set_title('Varying Color')
+
+#  Varying line width along a streamline
+ax2 = fig.add_subplot(gs[1, 0])
+lw = 5*speed / speed.max()
+ax2.streamplot(X, Y, U, V, density=0.6, color='k', linewidth=lw)
+ax2.set_title('Varying Line Width')
+
+# Controlling the starting points of the streamlines
+seed_points = np.array([[-2, -1, 0, 1, 2, -1], [-2, -1,  0, 1, 2, 2]])
+
+ax3 = fig.add_subplot(gs[1, 1])
+strm = ax3.streamplot(X, Y, U, V, color=U, linewidth=2,
+                      cmap='autumn', start_points=seed_points.T)
+fig.colorbar(strm.lines)
+ax3.set_title('Controlling Starting Points')
+
+# Displaying the starting points with blue symbols.
+ax3.plot(seed_points[0], seed_points[1], 'bo')
+ax3.set(xlim=(-w, w), ylim=(-w, w))
+
+# Create a mask
+mask = np.zeros(U.shape, dtype=bool)
+mask[40:60, 40:60] = True
+U[:20, :20] = np.nan
+U = np.ma.array(U, mask=mask)
+
+ax4 = fig.add_subplot(gs[2:, :])
+ax4.streamplot(X, Y, U, V, color='r')
+ax4.set_title('Streamplot with Masking')
+
+ax4.imshow(~mask, extent=(-w, w, -w, w), alpha=0.5, cmap='gray', aspect='auto')
+ax4.set_aspect('equal')
+
+plt.tight_layout()
+plt.show()
+
+# ==== Ellipses
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.patches import Ellipse
+
+# Fixing random state for reproducibility
+np.random.seed(19680801)
+
+NUM = 250
+
+ells = [Ellipse(xy=np.random.rand(2) * 10,
+                width=np.random.rand(), height=np.random.rand(),
+                angle=np.random.rand() * 360)
+        for i in range(NUM)]
+
+fig, ax = plt.subplots(subplot_kw={'aspect': 'equal'})
+for e in ells:
+    ax.add_artist(e)
+    e.set_clip_box(ax.bbox)
+    e.set_alpha(np.random.rand())
+    e.set_facecolor(np.random.rand(3))
+
+ax.set_xlim(0, 10)
+ax.set_ylim(0, 10)
+
+plt.show()
+
+# ---- Ellipse Rotated
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.patches import Ellipse
+
+angle_step = 45  # degrees
+angles = np.arange(0, 180, angle_step)
+
+fig, ax = plt.subplots(subplot_kw={'aspect': 'equal'})
+
+for angle in angles:
+    ellipse = Ellipse((0, 0), 4, 2, angle=angle, alpha=0.1)
+    ax.add_artist(ellipse)
+
+ax.set_xlim(-2.2, 2.2)
+ax.set_ylim(-2.2, 2.2)
+
+plt.show()
+
+# ==== Bar charts
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+from collections import namedtuple
+
+np.random.seed(42)
+
+Student = namedtuple('Student', ['name', 'grade', 'gender'])
+Score = namedtuple('Score', ['score', 'percentile'])
+
+# GLOBAL CONSTANTS
+test_names = ['Pacer Test', 'Flexed Arm\n Hang', 'Mile Run', 'Agility',
+              'Push Ups']
+test_units = dict(zip(test_names, ['laps', 'sec', 'min:sec', 'sec', '']))
+
+def attach_ordinal(num):
+    """Convert an integer to an ordinal string, e.g. 2 -> '2nd'."""
+    suffixes = {str(i): v
+                for i, v in enumerate(['th', 'st', 'nd', 'rd', 'th',
+                                       'th', 'th', 'th', 'th', 'th'])}
+    v = str(num)
+    # special case early teens
+    if v in {'11', '12', '13'}:
+        return v + 'th'
+    return v + suffixes[v[-1]]
+
+
+def format_score(score, test):
+    """
+    Create score labels for the right y-axis as the test name followed by the
+    measurement unit (if any), split over two lines.
+    """
+    unit = test_units[test]
+    if unit:
+        return f'{score}\n{unit}'
+    else:  # If no unit, don't include a newline, so that label stays centered.
+        return score
+
+
+def format_ycursor(y):
+    y = int(y)
+    if y < 0 or y >= len(test_names):
+        return ''
+    else:
+        return test_names[y]
+
+
+def plot_student_results(student, scores, cohort_size):
+    fig, ax1 = plt.subplots(figsize=(9, 7))  # Create the figure
+    fig.subplots_adjust(left=0.115, right=0.88)
+    fig.canvas.manager.set_window_title('Eldorado K-8 Fitness Chart')
+
+    pos = np.arange(len(test_names))
+
+    rects = ax1.barh(pos, [scores[k].percentile for k in test_names],
+                     align='center',
+                     height=0.5,
+                     tick_label=test_names)
+
+    ax1.set_title(student.name)
+
+    ax1.set_xlim([0, 100])
+    ax1.xaxis.set_major_locator(MaxNLocator(11))
+    ax1.xaxis.grid(True, linestyle='--', which='major',
+                   color='grey', alpha=.25)
+
+    # Plot a solid vertical gridline to highlight the median position
+    ax1.axvline(50, color='grey', alpha=0.25)
+
+    # Set the right-hand Y-axis ticks and labels
+    ax2 = ax1.twinx()
+
+    # Set the tick locations
+    ax2.set_yticks(pos)
+    # Set equal limits on both yaxis so that the ticks line up
+    ax2.set_ylim(ax1.get_ylim())
+
+    # Set the tick labels
+    ax2.set_yticklabels([format_score(scores[k].score, k) for k in test_names])
+
+    ax2.set_ylabel('Test Scores')
+
+    xlabel = ('Percentile Ranking Across {grade} Grade {gender}s\n'
+              'Cohort Size: {cohort_size}')
+    ax1.set_xlabel(xlabel.format(grade=attach_ordinal(student.grade),
+                                 gender=student.gender.title(),
+                                 cohort_size=cohort_size))
+
+    rect_labels = []
+    # Lastly, write in the ranking inside each bar to aid in interpretation
+    for rect in rects:
+        # Rectangle widths are already integer-valued but are floating
+        # type, so it helps to remove the trailing decimal point and 0 by
+        # converting width to int type
+        width = int(rect.get_width())
+
+        rank_str = attach_ordinal(width)
+        # The bars aren't wide enough to print the ranking inside
+        if width < 40:
+            # Shift the text to the right side of the right edge
+            xloc = 5
+            # Black against white background
+            clr = 'black'
+            align = 'left'
+        else:
+            # Shift the text to the left side of the right edge
+            xloc = -5
+            # White on magenta
+            clr = 'white'
+            align = 'right'
+
+        # Center the text vertically in the bar
+        yloc = rect.get_y() + rect.get_height() / 2
+        label = ax1.annotate(
+            rank_str, xy=(width, yloc), xytext=(xloc, 0),
+            textcoords="offset points",
+            horizontalalignment=align, verticalalignment='center',
+            color=clr, weight='bold', clip_on=True)
+        rect_labels.append(label)
+
+    # Make the interactive mouse over give the bar title
+    ax2.fmt_ydata = format_ycursor
+    # Return all of the artists created
+    return {'fig': fig,
+            'ax': ax1,
+            'ax_right': ax2,
+            'bars': rects,
+            'perc_labels': rect_labels}
+
+
+student = Student('Johnny Doe', 2, 'boy')
+scores = dict(zip(
+    test_names,
+    (Score(v, p) for v, p in
+     zip(['7', '48', '12:52', '17', '14'],
+         np.round(np.random.uniform(0, 100, len(test_names)), 0)))))
+cohort_size = 62  # The number of other 2nd grade boys
+
+arts = plot_student_results(student, scores, cohort_size)
+plt.show()
+
+# ==== Pie charts
+import matplotlib.pyplot as plt
+
+# Pie chart, where the slices will be ordered and plotted counter-clockwise:
+labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
+sizes = [15, 30, 45, 10]
+explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+        shadow=True, startangle=90)
+ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+plt.show()
+
+# ==== Tables
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+data = [[ 66386, 174296,  75131, 577908,  32015],
+        [ 58230, 381139,  78045,  99308, 160454],
+        [ 89135,  80552, 152558, 497981, 603535],
+        [ 78415,  81858, 150656, 193263,  69638],
+        [139361, 331509, 343164, 781380,  52269]]
+
+columns = ('Freeze', 'Wind', 'Flood', 'Quake', 'Hail')
+rows = ['%d year' % x for x in (100, 50, 20, 10, 5)]
+
+values = np.arange(0, 2500, 500)
+value_increment = 1000
+
+# Get some pastel shades for the colors
+colors = plt.cm.BuPu(np.linspace(0, 0.5, len(rows)))
+n_rows = len(data)
+
+index = np.arange(len(columns)) + 0.3
+bar_width = 0.4
+
+# Initialize the vertical-offset for the stacked bar chart.
+y_offset = np.zeros(len(columns))
+
+# Plot bars and create text labels for the table
+cell_text = []
+for row in range(n_rows):
+    plt.bar(index, data[row], bar_width, bottom=y_offset, color=colors[row])
+    y_offset = y_offset + data[row]
+    cell_text.append(['%1.1f' % (x / 1000.0) for x in y_offset])
+# Reverse colors and text labels to display the last value at the top.
+colors = colors[::-1]
+cell_text.reverse()
+
+# Add a table at the bottom of the axes
+the_table = plt.table(cellText=cell_text,
+                      rowLabels=rows,
+                      rowColours=colors,
+                      colLabels=columns,
+                      loc='bottom')
+
+# Adjust layout to make room for the table:
+plt.subplots_adjust(left=0.2, bottom=0.2)
+
+plt.ylabel("Loss in ${0}'s".format(value_increment))
+plt.yticks(values * value_increment, ['%d' % val for val in values])
+plt.xticks([])
+plt.title('Loss by Disaster')
+
+plt.show()
+
+# ==== Scatter plots
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cbook as cbook
+
+# Load a numpy record array from yahoo csv data with fields date, open, close,
+# volume, adj_close from the mpl-data/example directory. The record array
+# stores the date as an np.datetime64 with a day unit ('D') in the date column.
+price_data = (cbook.get_sample_data('goog.npz', np_load=True)['price_data']
+              .view(np.recarray))
+price_data = price_data[-250:]  # get the most recent 250 trading days
+
+delta1 = np.diff(price_data.adj_close) / price_data.adj_close[:-1]
+
+# Marker size in units of points^2
+volume = (15 * price_data.volume[:-2] / price_data.volume[0])**2
+close = 0.003 * price_data.close[:-2] / 0.003 * price_data.open[:-2]
+
+fig, ax = plt.subplots()
+ax.scatter(delta1[:-1], delta1[1:], c=close, s=volume, alpha=0.5)
+
+ax.set_xlabel(r'$\Delta_i$', fontsize=15)
+ax.set_ylabel(r'$\Delta_{i+1}$', fontsize=15)
+ax.set_title('Volume and percent change')
+
+ax.grid(True)
+fig.tight_layout()
+
+plt.show()
+
+# ==== GUI widgets
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider, Button
+
+
+# The parametrized function to be plotted
+def f(t, amplitude, frequency):
+    return amplitude * np.sin(2 * np.pi * frequency * t)
+
+t = np.linspace(0, 1, 1000)
+
+# Define initial parameters
+init_amplitude = 5
+init_frequency = 3
+
+# Create the figure and the line that we will manipulate
+fig, ax = plt.subplots()
+line, = plt.plot(t, f(t, init_amplitude, init_frequency), lw=2)
+ax.set_xlabel('Time [s]')
+
+axcolor = 'lightgoldenrodyellow'
+ax.margins(x=0)
+
+# adjust the main plot to make room for the sliders
+plt.subplots_adjust(left=0.25, bottom=0.25)
+
+# Make a horizontal slider to control the frequency.
+axfreq = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
+freq_slider = Slider(
+    ax=axfreq,
+    label='Frequency [Hz]',
+    valmin=0.1,
+    valmax=30,
+    valinit=init_frequency,
+)
+
+# Make a vertically oriented slider to control the amplitude
+axamp = plt.axes([0.1, 0.25, 0.0225, 0.63], facecolor=axcolor)
+amp_slider = Slider(
+    ax=axamp,
+    label="Amplitude",
+    valmin=0,
+    valmax=10,
+    valinit=init_amplitude,
+    orientation="vertical"
+)
+
+
+# The function to be called anytime a slider's value changes
+def update(val):
+    line.set_ydata(f(t, amp_slider.val, freq_slider.val))
+    fig.canvas.draw_idle()
+
+
+# register the update function with each slider
+freq_slider.on_changed(update)
+amp_slider.on_changed(update)
+
+# Create a `matplotlib.widgets.Button` to reset the sliders to initial values.
+resetax = plt.axes([0.8, 0.025, 0.1, 0.04])
+button = Button(resetax, 'Reset', color=axcolor, hovercolor='0.975')
+
+
+def reset(event):
+    freq_slider.reset()
+    amp_slider.reset()
+button.on_clicked(reset)
+
+plt.show()
